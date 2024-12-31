bda3e73f6964bcf1e5908ba36b48a4ec7d2e0979
I-Am-Nobel <jonobelmusonga@gmail.com>
2024-12-31 14:38:02 +0100

    update

---
+++ Assets/js/Project.js
@@ -0,0 +1,185 @@
+const VoirMoins=(index,commiter)=>{
+    const Fermer=()=>{
+        console.log('Wesh wesh ! : '+`${'voirMoins'+index}`)
+        const voirMoins=document.getElementById(`${'voirMoins'+index}`)
+        const voirtout=document.getElementById(`${'voirtout'+index}`)
+        const voirOpt=document.getElementById(`${'voirOpt'+index}`)
+        const voirPlus=document.getElementById(`${'voirPlus'+index}`)
+    
+        voirtout.style.display='none'
+        voirPlus.style.display='block'
+        voirOpt.style.display='block'
+        voirMoins.style.display='none'
+    }
+
+    if(commiter){
+        return Fermer()
+    }
+    else{
+        const voirMoins=document.getElementById(`${'voirMoins'+index}`)
+        voirMoins.addEventListener('click',()=>{
+            Fermer()
+        })
+    }
+}
+
+
+const Mypic = [  
+{  
+ImgSrc: './Assets/img/projets/Nobel_s_Project_Connected_Farm.png',  
+Legend: 'Ce projet vise a creer une plateforme permettant a rendre une ferme completement connecté. <br> <span style="font-weight:bold">Technologies utilisés</span> : Node.js,express,React,MongoDb,Socket.io et d\'autres technologies.<br> <span style="font-weight:bold">Langages utilisés</span> : HTML,CSS,JAVASCRIPT,TYPESCRIPT et PYTHON',
+ShowLegend:()=>{return Mypic[0].Legend.length>100? ` ${Mypic[0].Legend.substring(0,100)} ...Voir Plus`:Mypic[0].Legend},
+VidSrc: './Assets/Videos/FermeConnecté.mp4',  
+ZipVidSrc:'./Assets/FichiersZip/ProjetFermeConnecté_By_Nobel.rar',
+ViewVid:'./Video.html',
+urlVid:"./Videos.html/'./Assets/Videos/FermeConnecté.mp4'"
+},
+// {  
+// ImgSrc: './Assets/img/projets/Nobel_s_Project_Connected_Farm.png',  
+// Legend: 'Ce projet vise a creer une plateforme permettant a rendre une ferme completement connecté. <br> <span style="font-weight:bold">Technologies utilisés</span> : Node.js,express,React,MongoDb,Socket.io et d\'autres technologies.<br> <span style="font-weight:bold">Langages utilisés</span> : HTML,CSS,JAVASCRIPT,TYPESCRIPT et PYTHON',
+// ShowLegend:()=>{return Mypic[0].Legend.length>100? ` ${Mypic[0].Legend.substring(0,100)} ...Voir Plus`:Mypic[0].Legend},
+// VidSrc: './Assets/Videos/FermeConnecté.mp4',  
+// ZipVidSrc:'./Assets/FichiersZip/ProjetFermeConnecté_By_Nobel.rar',
+// ViewVid:'./Video.html',
+// urlVid:"./Videos.html/'./Assets/Videos/FermeConnecté.mp4'"
+// },
+// {  
+// ImgSrc: './Assets/img/projets/Nobel_s_Project_Connected_Farm.png',  
+// Legend: 'Ce projet vise a creer une plateforme permettant a rendre une ferme completement connecté. <br> <span style="font-weight:bold">Technologies utilisés</span> : Node.js,express,React,MongoDb,Socket.io et d\'autres technologies.<br> <span style="font-weight:bold">Langages utilisés</span> : HTML,CSS,JAVASCRIPT,TYPESCRIPT et PYTHON',
+// ShowLegend:()=>{return Mypic[0].Legend.length>100? ` ${Mypic[0].Legend.substring(0,100)} ...Voir Plus`:Mypic[0].Legend},
+// VidSrc: './Assets/Videos/FermeConnecté.mp4',  
+// ZipVidSrc:'./Assets/FichiersZip/ProjetFermeConnecté_By_Nobel.rar',
+// ViewVid:'./Video.html',
+// urlVid:"./Videos.html/'./Assets/Videos/FermeConnecté.mp4'"
+// },
+// {  
+// ImgSrc: './Assets/img/projets/Nobel_s_Project_Connected_Farm.png',  
+// Legend: 'Ce projet vise a creer une plateforme permettant a rendre une ferme completement connecté. <br> <span style="font-weight:bold">Technologies utilisés</span> : Node.js,express,React,MongoDb,Socket.io et d\'autres technologies.<br> <span style="font-weight:bold">Langages utilisés</span> : HTML,CSS,JAVASCRIPT,TYPESCRIPT et PYTHON',
+// ShowLegend:()=>{return Mypic[0].Legend.length>100? ` ${Mypic[0].Legend.substring(0,100)} ...Voir Plus`:Mypic[0].Legend},
+// VidSrc: './Assets/Videos/FermeConnecté.mp4',  
+// ZipVidSrc:'./Assets/FichiersZip/ProjetFermeConnecté_By_Nobel.rar',
+// ViewVid:'./Video.html',
+// urlVid:"./Videos.html/'./Assets/Videos/FermeConnecté.mp4'"
+// },
+// {  
+// ImgSrc: './Assets/img/projets/Nobel_s_Project_Connected_Farm.png',  
+// Legend: 'Ce projet vise a creer une plateforme permettant a rendre une ferme completement connecté. <br> <span style="font-weight:bold">Technologies utilisés</span> : Node.js,express,React,MongoDb,Socket.io et d\'autres technologies.<br> <span style="font-weight:bold">Langages utilisés</span> : HTML,CSS,JAVASCRIPT,TYPESCRIPT et PYTHON',
+// ShowLegend:()=>{return Mypic[0].Legend.length>100? ` ${Mypic[0].Legend.substring(0,100)} ...Voir Plus`:Mypic[0].Legend},
+// VidSrc: './Assets/Videos/FermeConnecté.mp4',  
+// ZipVidSrc:'./Assets/FichiersZip/ProjetFermeConnecté_By_Nobel.rar',
+// ViewVid:'./Video.html',
+// urlVid:"./Videos.html/'./Assets/Videos/FermeConnecté.mp4'"
+// },
+// {  
+// ImgSrc: './Assets/img/projets/Nobel_s_Project_Connected_Farm.png',  
+// Legend: 'Ce projet vise a creer une plateforme permettant a rendre une ferme completement connecté. <br> <span style="font-weight:bold">Technologies utilisés</span> : Node.js,express,React,MongoDb,Socket.io et d\'autres technologies.<br> <span style="font-weight:bold">Langages utilisés</span> : HTML,CSS,JAVASCRIPT,TYPESCRIPT et PYTHON',
+// ShowLegend:()=>{return Mypic[0].Legend.length>100? ` ${Mypic[0].Legend.substring(0,100)} ...Voir Plus`:Mypic[0].Legend},
+// VidSrc: './Assets/Videos/FermeConnecté.mp4',  
+// ZipVidSrc:'./Assets/FichiersZip/ProjetFermeConnecté_By_Nobel.rar',
+// ViewVid:'./Video.html',
+// urlVid:"./Videos.html/'./Assets/Videos/FermeConnecté.mp4'"
+// },
+];  
+console.log(Mypic[0].ShowLegend())
+Mypic.forEach((img, index) => {  
+const imgBoucle = document.createElement("div");  
+imgBoucle.innerHTML = `  
+
+<div id=${'Video'+index}  class="vid-parent mb-2 w-100 rounded px-1 " 
+
+style="width: 100%;background: linear-gradient(#fff,#05b105c4,#7e7e7e8e);">  
+<div class="container d-flex flex-wrap justify-content-space-between rounded-top dwn">  
+    <p>
+    </p>
+    <i style="cursor: pointer;" class="OpenDwn bi bi-list fs-2 text-dark"></i>  
+</div>  
+
+<div class="DownloadMenu container bg-success  rounded dlwn"  
+    style="display: none;padding:'5px'">  
+    <div  class="DownloadVid dlwdPic d-flex"> <i style='color:#fff;' class="bi bi-download"></i>  <i style='color:#fff;' class="bi bi-file-play"></i> <p style="color:#fff; margin-left: 10px;"> video</p></div>  
+    <div class="DownloadZip dlwdPic d-flex"> <i style='color:#fff;' class="bi bi-download"></i>  <i style='color:#fff;' class="bi bi-file-zip"></i> <p style="color:#fff;margin-left: 10px;"> video compressé</p></div> 
+</div> 
+<p
+style="text-align:center"
+>Cliquez la photo pour lire la video</p> 
+<div
+class=' justify-content-center d-flex'
+>  
+
+<a href=${img.ViewVid}?src='${img.VidSrc}'&&preview='${img.ImgSrc}''>
+    <img class="vid img-fluid " src="${img.ImgSrc}" alt="">  
+</a>  
+</div> 
+<div>  
+        <div class="container d-flex flex-wrap text-dark mt-2">  
+            
+            <p id=${'voirOpt'+index} class="voirOpt msi-2" style="font-size: small;">
+                ${(()=>{return img.Legend.length>100? ` ${img.Legend.substring(0,100)}
+                     <p style='font-size: small;color:blue' id=${'voirPlus'+index} class='voirPlus'>Voir Plus</p>`:img.Legend})()}  
+
+            </p>  
+            <p class="voirtout msi-2" id=${'voirtout'+index}  style='display:none;font-size: small;' >
+                ${img.Legend} <p class='voirMoins' id=${'voirMoins'+index} style='display:none;font-size: small;color:blue;'>Voir moins</p>
+            </p>  
+        </div>  
+    </div>   
+</div>  
+`;  
+
+
+const imgParent = document.querySelector('.Videos');  
+imgParent.appendChild(imgBoucle);  
+
+
+
+const openDwn = imgBoucle.querySelector('.OpenDwn');  
+openDwn.addEventListener('click', () => { 
+    console.log('Wesh wesh dwnload ! : '+`${'voirPlus'+index}`) 
+const downloadMenu = imgBoucle.querySelector('.DownloadMenu');  
+downloadMenu.style.display == 'block' ? downloadMenu.style.display = 'none' : downloadMenu.style.display = 'block';  
+VoirMoins(index,1)
+});  
+
+const DownloadVid = imgBoucle.querySelector('.DownloadVid'); 
+
+if (DownloadVid){
+DownloadVid.addEventListener('click', () => {  
+const link = document.createElement('a');    
+link.href = img.VidSrc;  
+link.download = img.VidSrc.split('/').pop();  
+link.click(); 
+const openDwn = imgBoucle.querySelector('.OpenDwn');
+openDwn.click() 
+}); 
+} 
+const downloadZip = imgBoucle.querySelector('.DownloadZip');  
+if (downloadZip){
+        downloadZip.addEventListener('click', () => {  
+        const link = document.createElement('a');  
+        link.href =img.ZipVidSrc;  
+        link.download = img.ZipVidSrc.split('/').pop();  
+        link.click();  
+        const openDwn = imgBoucle.querySelector('.OpenDwn');
+        openDwn.click()
+        }); 
+} 
+
+// VOIR PLUS/MOINS
+
+const VoirPlus=document.getElementById(`${'voirPlus'+index}`)
+
+VoirPlus.addEventListener('click',()=>{
+        const voirtout=document.getElementById(`${'voirtout'+index}`)
+        const voirOpt=document.getElementById(`${'voirOpt'+index}`)
+        const voirMoins=document.getElementById(`${'voirMoins'+index}`)
+        const downloadMenu = imgBoucle.querySelector('.DownloadMenu');  
+        downloadMenu.style.display == 'block' && (downloadMenu.style.display = 'none' )
+        const Video = imgParent.querySelector('.vid-parent') 
+        Video.style.maxWidth='auto'
+        voirtout.style.display='block'
+        voirMoins.style.display='block'
+        voirOpt.style.display='none'
+        VoirPlus.style.display='none'
+})
+VoirMoins(index)
+});  
---
+++ Assets/js/preference.js
@@ -0,0 +1,4 @@
+// window.addEventListener('load',()=>{
+//     console.log('windows :', document.body.style.backgroundColor)
+//     document.body.style.backgroundColor='#444'
+// })
